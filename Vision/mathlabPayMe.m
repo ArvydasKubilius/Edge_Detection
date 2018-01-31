74f16d68fdc8d39f0f5082e90bcbab31ab36d757
ArvydasKubilius <arvydaskub@gmail.com>
2018-01-31 15:49:10 +0000

    Update and rename OwnZeroCrossing.m to mathlabPayMe.m

--- Vision/mathlabPayMe.m
+++ Vision/mathlabPayMe.m
@@ -1,12 +1,11 @@
 function [zc] = mathlabPayMe(img, t)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%Own zero crossing since the MathLab one is very bad
 zc = zeros(1024,1280);
 for i= 2:1:1499
     for j = 2:1:1116
         if ((abs ((img(i-1,j) - img(i+1,j)))) > t)
             zc(i,j) = 1;
-           % zc(i-1,j) = 1;
+           %zc(i-1,j) = 1;
             %zc(i+1,j) = 1;
         elseif((abs ((img(i,j-1) - img(i,j+1)))) > t)
             zc(i,j) = 1;
